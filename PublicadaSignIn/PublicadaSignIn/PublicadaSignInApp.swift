cba40d09fa3d1f51ef0859162b4d5fa5b8578b58
Filipe Reis <reislipe@protonmail.com>
2024-09-19 15:30:10 -0300

    Creating Xcode Project

---
+++ PublicadaSignIn/PublicadaSignIn/PublicadaSignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicadaSignInApp.swift
+//  PublicadaSignIn
+//
+//  Created by Joao Filipe Reis Justo da Silva on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PublicadaSignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
